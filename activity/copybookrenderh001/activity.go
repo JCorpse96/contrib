ee45348173568d9168ead3c9839fc66aeb53c258
JCorpse96 <joaocrespo96@gmail.com>
2023-02-17 10:23:20 +0000

    Change reference of core/data in rendercopybookh001

--- activity/copybookrenderh001/activity.go
+++ activity/copybookrenderh001/activity.go
@@ -3,9 +3,9 @@ package copybookrenderh001
 import (
 	"encoding/json"
 
-	"github.com/JCorpse96/core/data/coerce"
 	"github.com/JCorpse96/core/data/copybook"
 	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/coerce"
 )
 
 func init() {
@@ -86,13 +86,13 @@ func (a *Activity) Metadata() *activity.Metadata {
 // Eval implements api.Activity.Eval - Logs the Message
 func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
+	h001 := initialize()
+
 	input := &Input{}
 	err = ctx.GetInputObject(input)
 	if err != nil {
 		return true, err
 	}
-
-	h001 := initialize()
 
 	var message interface{}
 	message, _ = coerce.ToObject(input.Request)
